4b1c0b5b25baed8c6217216ee885a47ce2eb8b22
Brian Quinlan <bquinlan8@yahoo.com>
2017-02-02 16:07:48 -0600

    trying to add animated scrolling and added another showcase

--- page.js
+++ page.js
@@ -1,32 +1,31 @@
         
     //changes header background after getting to end of header picture
     //found on stackoverflow
-	var headerBottom = $('#header-bg').height() + 150;
+    var headerBottom = $('#header-bg').height() + 150;
 
     //on scroll
-	$(window).on('scroll', function(){
-	var stop = Math.round($(window).scrollTop());
+    $(window).on('scroll', function(){
+        var stop = Math.round($(window).scrollTop());
 
-	if (stop > headerBottom) {
-		$('.nav-style').addClass('after-header');
-	} else {
-		$('.nav-style').removeClass('after-header');
-	}
-	});
-
-    
-    //close navbar after a selection
-    $('.navbar-nav a').click(function(){
-	$('.collapse').collapse('hide');
+        if (stop > headerBottom) {
+	    $('.nav-style').addClass('after-header');
+        } else {
+	    $('.nav-style').removeClass('after-header');
+        }
     });
 
-    
+
+    //close navbar after a selection
+    $('.navbar-nav a').click(function(){
+        $('.collapse').collapse('hide');
+    });
+
     //smaller icons for phones 
     if (document.documentElement.clientWidth < 500) {
         var phone = document.getElementById('phone');
-    	phone.src = 'media/icons/phone-sm.png';
+        phone.src = 'media/icons/phone-sm.png';
 
-	var email = document.getElementById('email');
+        var email = document.getElementById('email');
 	email.src = 'media/icons/email-sm.png';
 
 	var github = document.getElementById('github');
